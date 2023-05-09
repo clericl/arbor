0cb324f0930b44f3125144a1f3a7bf326e31684e
Eric L <eliang58@gmail.com>
2023-05-09 12:35:29 -0400

    Revert "add key authorization"
    This reverts commit 59dc24aad50e13ed365541236dc31623554d946c.

--- src/utils/BigQuery.js
+++ src/utils/BigQuery.js
@@ -1,18 +1,18 @@
 class BigQuery {
   static async getRandomSeed(limit = 1) {
-    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/random/' + limit + `?key=${process.env.REACT_APP_API_KEY}`)
+    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/random/' + limit)
     const body = await res.json()
     return body
   }
 
   static async fetchTree(source){
-    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/trees/' + source + `?key=${process.env.REACT_APP_API_KEY}`)
+    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/trees/' + source)
     const body = await res.json()
     return body
   }
 
   static async saveTree(network){
-    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + `/trees?key=${process.env.REACT_APP_API_KEY}`, {
+    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/trees', {
       method: 'POST',
       mode: 'cors',
       headers: {
@@ -26,25 +26,25 @@ class BigQuery {
   }
 
   static async fetchRelations(seed) {
-    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/relations/' + seed + `?key=${process.env.REACT_APP_API_KEY}`)
+    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/relations/' + seed)
     const body = await res.json()
     return body
   }
 
   static async fetchParents(seed) {
-    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/parents/' + seed + `?key=${process.env.REACT_APP_API_KEY}`)
+    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/parents/' + seed)
     const body = await res.json()
     return body
   }
 
   static async fetchDescendants(seed, limit) {
-    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/descendants/' + seed + `?limit=${limit}&key=${process.env.REACT_APP_API_KEY}`)
+    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/descendants/' + seed + `?limit=${limit}`)
     const body = await res.json()
     return body
   }
 
   static async fetchAll(seed) {
-    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/all/' + seed + `?key=${process.env.REACT_APP_API_KEY}`)
+    const res = await fetch(process.env.REACT_APP_API_ENDPOINT + '/all/' + seed)
     const body = await res.json()
     return body
   }
